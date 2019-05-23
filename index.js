a6265edc35935e265f7de96b8f96745e56b5b90c
pengdaosong <pengdaosong@medlinker.com>
2019-05-23 09:26:34 +0800

    rn

--- index.js
+++
@@ -1,9 +0,0 @@
-/** @format */
-
-import {AppRegistry} from 'react-native';
-import {name as appName} from './app.json';
-import setup from './js/page/setup'
-import input from './js/component/InputTextView'
-import bar from './js/page/HomeActivity'
-
-AppRegistry.registerComponent(appName, () => bar);
